65145d6273ee521913ff2677c8e47104864f831c
pasquaa <loripasquarelli@gmail.com>
2023-06-21 14:34:53 +0200

    commit iniziale

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Form } from "./components/From";
+
+export default function App() {
+  return(
+    <div className="max-w-7xl mx-auto py-4 px-8">
+    <div className="max-w-3-xl">
+      <Form />
+    </div>
+    </div>
+  )
+}
